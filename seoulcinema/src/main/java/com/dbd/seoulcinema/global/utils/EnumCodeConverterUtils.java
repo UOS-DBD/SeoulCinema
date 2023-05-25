9463575010bb9983020858fca3534d6c510e2140
Lee sang min <dkwkghkdlxld2@gmail.com>
2023-05-26 04:02:38 +0900

    feat: 코드와 enum 매핑 시키는 utils 클래스 생성

---
+++ seoulcinema/src/main/java/com/dbd/seoulcinema/global/utils/EnumCodeConverterUtils.java
@@ -0,0 +1,35 @@
+package com.dbd.seoulcinema.global.utils;
+
+import com.dbd.seoulcinema.domain.enumeration.CommonDescCode;
+import com.dbd.seoulcinema.global.exception.DataNotFoundException;
+import lombok.NoArgsConstructor;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.EnumSet;
+
+
+@NoArgsConstructor
+public class EnumCodeConverterUtils {
+
+    public static <T extends Enum<T> & CommonDescCode> T getEnumFromCode(Class<T> targetEnum, String code) {
+
+        if (StringUtils.isEmptyOrWhitespace(code)) {
+            return null;
+        }
+
+        return EnumSet.allOf(targetEnum)
+                .stream()
+                .filter(f -> f.getCode().equals(code))
+                .findAny()
+                .orElseThrow(() -> new DataNotFoundException());
+    }
+
+
+    public static <T extends Enum<T> & CommonDescCode> String getCodeFromEnum(T enumType) {
+        if(enumType == null){
+            return "";
+        }
+
+        return enumType.getCode();
+    }
+}
